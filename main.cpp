5e46769cfdcd05dbfd1e18defc5766aac3fdfca9
Kurt Krueckeberg <kurtk@pobox.com>
2014-11-10 12:20:27 -0600

    reverted code to prior form

--- main.cpp
+++ main.cpp
@@ -6,22 +6,19 @@
  */
 
 #include <cstdlib>
-#include <array>
-#include "new-generic-merge-sort.h"
+#include "generic-merge-sort.h"
 #include "print-array.h"
-
 using namespace std;
 
 int main(int argc, char** argv) 
 {
-    array<int, 10> a{ 9, 8, 7, 5, 0, 4, 10, 2, 3, 1};
+    int a[] = { 9, 8, 7, 5, 0, 4, 10, 2, 3, 1};
     
-    int size = a.size();
+    int size = sizeof(a)/sizeof(int);
     
     merge_sort(a, 0, size - 1);
     
-    //--print_array(a, 0,  size - 1);     
-      
+    print_array(a, 0,  size - 1);     
     return 0;
 }
 
