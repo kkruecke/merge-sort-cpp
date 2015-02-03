cf0e3f6d114d33949752d04f53ec82dacd425276
Kurt Krueckeberg <kurtk@pobox.com>
2015-02-03 09:39:32 -0500

    more changes

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ int main(int argc, char** argv)
   int a[] = { 9, 8, 7, 5, 0, 4, 10, 2, 3, 1};
 
   int size = sizeof(a)/sizeof(int);
-
-  merge_sort(a, 0, size - 1);
+  
+  merge_sort(a, 0, size- 1);
 
   print_array(a, 0, size - 1);
-
+ 
   return 0;
 }
