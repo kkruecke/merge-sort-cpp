a6b9100499624e1d25755f5b3de17bdb944aca5b
Kurt Krueckeberg <kurtk@pobox.com>
2015-02-08 14:24:35 -0500

    updated animation code

--- animated-merge-sort.h
+++ animated-merge-sort.h
@@ -5,12 +5,29 @@
 #include <iostream>
 #include <functional>
 #include <iterator>
+#include <map>
 
-struct partition { 
+class partition { 
 
+public:    
+    
   enum section {none, left, right };
+  
+  std::string get_string(section s) const;
+  
+private:
+    static  std::map<section, std::string> mapping;
+};p
 
-};
+ partition::std::map<partition::section, std::string>::mapping { {none, std::string("none"), 
+                                                                  {left, std::string("left"),
+                                                                  {right, std::string("right")
+                                                                  };
+ 
+ inline std::string partition::get_string(partition::section s) const
+ {
+     return partition::mapping[s];
+ }
 
 template<typename Iterator> static void print_stdout(Iterator first, Iterator last, int depth, partition::section sec) 
 {
@@ -20,30 +37,30 @@ template<typename Iterator> static void print_stdout(Iterator first, Iterator la
 
     case partition::none:
         
-           part = std::string("whole data structure");           
+           part = std::string("whole");           
            break;
 
       case partition::left:
     
-           part = std::string("left half");           
+           part = std::string("left ");           
            break;
 
       case partition::right:
           
-           part = std::string("right half");           
+           part = std::string("right");           
            break;
   }
   
   std::cout << "At depth " << depth << ". Section:  " << part << ": ";
     
-    // Since last is the actual last element (and not one pass it), we  add one because copy() requires "one past".                          
-    std::copy(first, last + 1, std::ostream_iterator<decltype(*first)>(std::cout, " "));
+  // Since last is the actual last element (and not one pass it), we  add one because copy() requires "one past".                          
+  std::copy(first, last + 1, std::ostream_iterator<decltype(*first)>(std::cout, " "));
         
-    std::cout << std::endl;
+  std::cout << std::endl;
 }
+
 /*
- * An array iterator, which temp_buffer is, is different potentially of a different type than that the iterator types of parameters first, mid and last--all of
- * which could be different data structures. Therefore two iterator types are needed in the event that the data structure being sorted is not an array.
+ * Two iterator types are needed. The data structure being sorted may not an array.
  */
 template<typename Iterator_type1, typename Iterator_type2, typename Comparator> static void merge(Iterator_type1 first, Iterator_type1 mid,
         Iterator_type1 last,
@@ -77,16 +94,16 @@ template<typename Iterator_type1, typename Iterator_type2, typename Comparator> 
         Iterator_type1 mid_iterator = first + mid;
         Iterator_type1 mid_iterator_plus1 = mid_iterator + 1;
 
-        do_merge_sort(first, mid_iterator, buffer, c, depth + 1);    // sort left half
+        do_merge_sort(first, mid_iterator, buffer, c, depth + 1, partition::section::left);    // sort left half
 
-        do_merge_sort(mid_iterator_plus1, last, buffer, c, depth + 1); // sort right half
+        do_merge_sort(mid_iterator_plus1, last, buffer, c, depth + 1, partition::section::right); // sort right half
         
         // merge the two halves
         merge(first, mid_iterator, last, buffer, c, depth);
 
     } else {
 
-        std::cout << "Recursion base case. Depth  " << depth << "." <<std::endl;
+        std::cout << "Recursion base case. Depth  " << depth << ". Section: " << std::string(std::endl;
     }
 }
 
@@ -103,11 +120,11 @@ template<typename Iterator_type1, typename Iterator_type2, typename Comparator> 
 
     std::copy(first1, last1 + 1, std::ostream_iterator<decltype(*first1)>(std::cout, " "));
 
-    std::cout << " ], [ ";
+    std::cout << "], [ ";
 
     std::copy(first2, last2 + 1, std::ostream_iterator<decltype(*first1)>(std::cout, " "));
 
-    std::cout << " ] " <<  std::string(" ---> ") << std::endl;
+    std::cout << "] " <<  std::string(" ---> ");
 
     int index = 0;
     
@@ -144,6 +161,8 @@ template<typename Iterator_type1, typename Iterator_type2, typename Comparator> 
    // copy the temp array to the original array
    int length = last + 1 - first;
    Iterator_type2 start = buffer_start;
+
+   Iterator_type2 first_extra = first; 
    
     for (Iterator_type1 end = start + length; start != end; ++start) {
         
@@ -155,7 +174,7 @@ template<typename Iterator_type1, typename Iterator_type2, typename Comparator> 
     // TODO: range is wrong.
     // TODO: USe range syntax with iterators (in this case pointers).
 
-    std::copy(first, last + 1, std::ostream_iterator<decltype(*first)>(std::cout, " "));
+    std::copy(first_extra, first_extra + length, std::ostream_iterator<decltype(*first)>(std::cout, " "));
 
     std::cout << std::string(" ] ") << std::endl;
 }
