772ec972fc25a714fb29e6be58e29db8595e2635
Kurt Krueckeberg <kurtk@pobox.com>
2015-02-09 14:42:57 -0500

    updated animation code

--- animated-merge-sort.h
+++ animated-merge-sort.h
@@ -168,12 +168,14 @@ template<typename Iterator_type1, typename Iterator_type2, typename Comparator> 
     print_array(first_extra, first_extra + length);
 
     std::cout << std::endl;
-/*
-    std::cout << std::string(" [ "); 
-            
-    std::copy(first_extra, first_extra + length, std::ostream_iterator<decltype(*first)>(std::cout, " "));
 
-    std::cout << std::string(" ] ") << std::endl;
-*/
+    // print out merged array
+    /*
+    std::cout << std::string("Original input now: ");
+
+    print_array(first_extra, last );
+
+    std::cout << std::endl;
+    */
 }
 #endif
