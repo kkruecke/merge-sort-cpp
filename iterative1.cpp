da31dd6df6a6ced33e88267f7f7473be1cd75e58
Kurt Krueckeberg <kurtk@pobox.com>
2015-02-15 14:29:32 -0500

    code updates. See todo.txt and iterative.cpp

--- iterative1.cpp
+++ iterative1.cpp
@@ -1,122 +1,24 @@
-//-------------
 /*
-From:
-http://www.softwareandfinance.com/Visual_CPP/Merge_Sort_Iterative.html
-*/
-
-#include <iostream>
-#include <queue>
-#include <vector>
-
-// Usual merge/sort step
-void DoMerge(int numbers[], int left, int mid, int right)
+ * For an excellent explanation of this algorithm see:
+   http://www.mathcs.emory.edu/~cheung/Courses/171/Syllabus/7-Sort/merge-sort5.html
+ */
+template<typename T, Comparator comparer> T *merge_sort(T *a, Comaparator comparer)
 {
-    int temp[25];
+ int length = sizeof(a) / sizeof(T);
 
-    auto left_end = (mid - 1);
+    for (int i = 1; i <= length / 2 + 1; i *= 2)  {
 
-    auto tmp_pos = left;
-
-    auto num_elements = (right - left + 1);
-
-    while ((left <= left_end) && (mid <= right)) {
-
-        if (numbers[left] <= numbers[mid]) {
-
-            temp[tmp_pos++] = numbers[left++];
-
-        }  else {
-
-            temp[tmp_pos++] = numbers[mid++];
+        for (int j = i; j < length; j += 2 * i) {
+            
+            /*
+             * input: array, left, middle, right, comparator.
+             *
+             *
+             *
+             */ 
+            Merge(a, j - i, j, Math.Min(j + i, array.Length), comparer);
         }
     }
-
-    while (left <= left_end) {
-
-        temp[tmp_pos++] = numbers[left++];
-    }
-
-    while (mid <= right)  {
-
-        temp[tmp_pos++] = numbers[mid++];
-    }
-
-    for (auto i=0; i < num_elements; i++) {
-
-        numbers[right--] = temp[right];
-    }
-}
-
-struct MergePosInfo {
-
-    int left;
-    int mid;
-    int right;
-};
-
-void Merge_Sort_Iterative(int numbers[], int left, int right)
-{
-    int mid;
-
-    if (right <= left)
-        return;
-
-    std::vector<std::pair<int, int> > list;
-
-    std::vector<MergePosInfo> mlist;
-
-    list.push_back(std::pair<int, int>(left, right));
-
-    MergePosInfo info;
-
-    while(1) {
-
-        if(list.size() == 0)
-
-            break;
-
-        left = list.back().first;
-
-        right = list.back().second;
-
-        list.pop_back();
-
-        mid = (right + left) / 2;
-
-        if(left < right) {
-
-            info.left = left;
-
-            info.right = right;
-
-            info.mid = mid + 1;
-
-            mlist.push_back(info);
-
-            list.push_back(std::pair<int, int>(left, mid));
-
-            list.push_back(std::pair<int, int>((mid+1), right));
-        }
-    }
-
-    // Q: Why not simply use a stack and pop it at the end of the loop. 
-    for(int i = mlist.size() - 1; i >= 0; i--) {
-
-        DoMerge(numbers, mlist[i].left, mlist[i].mid, mlist[i].right);
-
-    }
-}
-
-int _tmain(int argc, _TCHAR* argv[])
-{
-    int arr[] = { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
-
-    int len = sizeof(arr) / sizeof(arr[0]);
-
-    Merge_Sort_iterative(arr, 0, len - 1);
-
-    for(int i = 0; i < len; i++)
-        std::cout << arr[i] << " ";
  
-     return 0;
+    return a;
 }
